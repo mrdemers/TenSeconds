1aafbba89aa6696f8b8604637bc05d2418274ff2
SarthakDesai <SarthakDesai@users.noreply.github.com>
2017-03-07 19:09:31 -0500

    Update GameApp.java
    - Added comments for easier credibility. This can help someone else looking at the program to understand the code.
    - Added class and method descriptions

--- src/com/GameApp.java
+++ src/com/GameApp.java
@@ -1,20 +1,35 @@
+/*
+ * Method Description: Class for allowing the game to be imbeded on a web page or to be viewed on java applet viewer
+*/
+	
 package com;
 
-import java.applet.Applet;
-import java.awt.BorderLayout;
+import java.applet.Applet; 		// import the java Applet class that allows the program to be imbedded onto a web page
+import java.awt.BorderLayout;		// import the java BorderLayout class for layout managent of the Applet window
 
 public class GameApp extends Applet{
-	private static final long serialVersionUID = 1L;
-	ShortGame game = new ShortGame();
+	private static final long serialVersionUID = 1L;	// serialVersionUID defined for valid program serialization/deserialization
+	ShortGame game = new ShortGame();			// creates a new instance of the game 
+	
+	/*
+	* Method Description: Method for intializing the 
+	*/
 	public void init() {
-		setLayout(new BorderLayout());
-		add(game, BorderLayout.CENTER);
+		setLayout(new BorderLayout());			// allows to implement a custom layout to the window
+		add(game, BorderLayout.CENTER);			// adds the game instance to the center space of the applet window
 	}
 	
-	public void start() {
+	
+	/*
+	* Method Description: Method for applet to start the execution 
+	*/
+	public void start() {			
 		game.start();
 	}
 	
+	/*
+	* Method Description: Method for applet to stop the execution
+	*/
 	public void stop() {
 		game.stop();
 	}
